19474af89c2f7a442bfb23c7df2e5e383cee6957
gmao0601 <66326436+gmao0601@users.noreply.github.com>
2020-10-17 17:17:46 -0500

    Update change to matlab later.py

--- change to matlab later.py
+++ change to matlab later.py
@@ -1,3 +1,11 @@
 #coefficient of drag
 
 def drag_coefficient(D, rho, v, A):
+  #D = drag
+  #rho = density of air(so this changes)
+  #v = velocity 
+  #A = surface area of rocket
+  #Cd = drag coeffient
+  
+  Cd = D / (a * .5 * rho * v^2)
+  return Cd
